71304043a8827884e03ba56d57a96234dbeea5e5
Santi Mollá <santi.molla@yahoo.es>
2017-07-27 10:42:46 +0200

    Añadido el id al modelo de las cuentas de usuario de la aplicación

--- src/main/java/com/starwars/model/Account.java
+++ src/main/java/com/starwars/model/Account.java
@@ -4,13 +4,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Account {
 
+    @Id
+    @GeneratedValue
     private Long id;
+    
     private String username;
     private String password;
 }
